9fed87c878e147718b8907290f99284a4a381abe
小孩子不可以打架 <id77@users.noreply.github.com>
2020-08-24 15:52:09 +0800

    完善脚本

--- Script/rrtv.js
+++ Script/rrtv.js
@@ -1,20 +1,29 @@
 /* 
+
+# QuanX
 [rewrite_local]
 # 人人视频 旧版本api限制解除
 ^https:\/\/api\.rr\.tv url script-request-header https://raw.githubusercontent.com/id77/QuantumultX/master/Script/rrtv.js
 
-[mitm]
+# Surge
+[Script]
+# 人人视频 旧版本api限制解除
+人人视频 旧版本api限制解除  = type=http-request,pattern=^https:\/\/api\.rr\.tv,script-path= https://raw.githubusercontent.com/id77/QuantumultX/master/Script/rrtv.js
+ 
 hostname = *.rr.tv
 */
 
-var headers = JSON.parse(
-  JSON.stringify($request.headers).replace(/4\.\d\.\d/g, '4.10.1')
+const reg = /4\.\d+\.\d/g;
+const headers = JSON.parse(
+  JSON.stringify($request.headers).replace(reg, '4.10.1')
 );
+
+headers.clientType= 'web';
+headers.clientVersion = '';
+headers['User-Agent'] = '';
 
 const data = { headers };
 
-if ($request.url) data.url = $request.url.replace(/4\.\d\.\d/g, '4.10.1');
-
-if ($request.path) data.path = $request.path.replace(/4\.\d\.\d/g, '4.10.1');
+data.url = $request.url.replace(reg, '4.10.1');
 
 $done(data);
